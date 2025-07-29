d2e44db67332126b5d5fffbd07e18586b0d09128
danmendz <daniel.mdz.33palacios@gmail.com>
2025-07-28 22:00:36 -0600

    [feat] Integración de paquete UI para mejorar el diseño del menú y dashboard del usuario

---
+++ resources/views/components/sidebar/content.blade.php
@@ -0,0 +1,54 @@
+<x-perfect-scrollbar
+    as="nav"
+    aria-label="main"
+    class="flex flex-col flex-1 gap-4 px-3"
+>
+
+    <x-sidebar.link
+        title="Dashboard"
+        href="{{ route('dashboard') }}"
+        :isActive="request()->routeIs('dashboard')"
+    >
+        <x-slot name="icon">
+            <x-icons.dashboard class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+        </x-slot>
+    </x-sidebar.link>
+
+    <x-sidebar.dropdown
+        title="Buttons"
+        :active="Str::startsWith(request()->route()->uri(), 'buttons')"
+    >
+        <x-slot name="icon">
+            <x-heroicon-o-bars-3-center-left class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+        </x-slot>
+
+        <x-sidebar.sublink
+            title="Text button"
+            href="{{ route('buttons.text') }}"
+            :active="request()->routeIs('buttons.text')"
+        />
+        <x-sidebar.sublink
+            title="Icon button"
+            href="{{ route('buttons.icon') }}"
+            :active="request()->routeIs('buttons.icon')"
+        />
+        <x-sidebar.sublink
+            title="Text with icon"
+            href="{{ route('buttons.text-icon') }}"
+            :active="request()->routeIs('buttons.text-icon')"
+        />
+    </x-sidebar.dropdown>
+
+    <div
+        x-transition
+        x-show="isSidebarOpen || isSidebarHovered"
+        class="text-sm text-gray-500"
+    >
+        Links
+    </div>
+
+    <x-sidebar.link title="Link 1" href="" />
+    <x-sidebar.link title="Link 2" href="" />
+    <x-sidebar.link title="Link 3" href="" />
+
+</x-perfect-scrollbar>
